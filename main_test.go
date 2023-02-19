3619b052f8b1f67e071e400cd606f28850c20047
Igor Melo <igoracmelo@protonmail.com>
2023-02-19 17:39:47 -0300

    fix: benchmark samples path

--- main_test.go
+++ main_test.go
@@ -8,16 +8,16 @@ import (
 
 func Benchmark_RunWithUnbufferedOutputs(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-    run([]string{"./xx", "samples/30M.txt"}, os.Stdout, os.Stderr)
+		run([]string{"./xx", "samples/10M"}, os.Stdout, os.Stderr)
 	}
 }
 
 func Benchmark_RunWithBufferedOutputs(b *testing.B) {
-  stdout := bufio.NewWriter(os.Stdout)
-  b.ResetTimer()
+	stdout := bufio.NewWriter(os.Stdout)
+	b.ResetTimer()
 
-  for i := 0; i < b.N; i++ {
-    run([]string{"./xx", "samples/30M.txt"}, stdout, os.Stderr)
-    stdout.Flush()
-  }
+	for i := 0; i < b.N; i++ {
+		run([]string{"./xx", "samples/10M"}, stdout, os.Stderr)
+		stdout.Flush()
+	}
 }
